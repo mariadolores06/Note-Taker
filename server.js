3d7c7ff1183dd80ca07d5c305510bebede5122ed
unknown <maria.rodriguez@northwestern.edu>
2023-05-26 22:34:13 -0500

    hide modules

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-// const { clog } = require('./middleware/clog');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
-
-
-// app.use(clog);
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
